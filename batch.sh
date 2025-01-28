598da19538b01ffbd2627e64283cad1f14712051
root <root@ip-172-31-27-151.ap-southeast-1.compute.internal>
2025-01-28 08:31:26 +0000

    20250128-083126

--- batch.sh
+++ batch.sh
@@ -5,5 +5,65 @@ dnf -y install mysql mysql-community-client
 dnf -y install mysql-community-server
 systemctl start mysqld
 
+dnf -y install httpd
+service httpd start
+cd /var/www/html
+touch index.html
 
+cat << EOF > index.html
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>テストタイトル</title>
+    </head>
+    <body>
+        <p>Hello AWS!</p>
+    </body>
+</html>
+
+EOF
+
+
+dnf -y install php php-mysqlnd php-mbstring
+
+sed -i 's@<IfModule mime_module>@<IfModule mime_module>\n    AddType application/x-httpd-php .php@g' /etc/httpd/conf/httpd.conf
+
+service httpd restart
+
+
+touch index.php
+
+cat << EOF > index.php
+
+<?php
+try
+{
+$dbs = "mysql:host=【エンドポイント】;dbname=testdb;charset=utf8";
+$user='【ユーザー名】';
+$password='【パスワード】';
+$dbh=new PDO($dbs, $user, $password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql='SELECT * FROM user';
+$stmt=$dbh->prepare($sql);
+
+$dbh=null;
+//実行
+$stmt->execute();
+
+$all = $stmt->fetchAll();
+//配列を表示
+foreach($all as $loop){
+  echo $loop['id']." ".$loop['name'].'<br>';
+}
+
+}
+catch (Exception $e)
+{
+        print '接続失敗';
+        exit();
+}
+?>
+EOF
 
---
+++ simple.sql
@@ -0,0 +1,5 @@
+create database testdb;
+create table testdb.user(id int , name varchar(20));
+insert into testdb.user VALUES (1,'tanaka'),(2,'satou');
+#select * from testdb.user;
+#quit
