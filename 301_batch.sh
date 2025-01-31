36bdfe4248cadd5e1c1e1f6f32fd1b24954993c7
root <root@ip-10-0-1-229.ap-southeast-1.compute.internal>
2025-01-31 07:55:38 +0000

    20250131-075538

--- 301_batch.sh
+++ 301_batch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 conda create -n renv python=3.9 -y
+
+source /root/anaconda3/etc/profile.d/conda.sh
 conda activate renv
 echo y | conda install -c r r-irkernel 
 echo y | conda install r-essentials
---
+++ batch3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /root/anaconda3/etc/profile.d/conda.sh
+
+conda deactivate
+
+
